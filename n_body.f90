1cd35ea74adc645854303d30a3ad9bacf02ce123
Lewis <130001684+Lewis-Adkins@users.noreply.github.com>
2023-08-05 13:03:32 -0400

    Add files via upload

--- n_body.f90
+++ n_body.f90
@@ -1,35 +1,35 @@
-subroutine distance(x1,y1,z1,x2,y2,z2,d) 
+subroutine distance(x1, y1, z1, x2, y2, z2, d)
     implicit none
     real, intent(in) :: x1, y1, x2, y2, z1, z2
     real             :: d
     
-    d = sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+    d = sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
 end subroutine distance
 
-subroutine f_grav(m1,m2,d, Force)
+subroutine f_grav(m1, m2, d, Force)
     implicit none
     real, intent(in) :: m1, m2, d
-    real             :: G = 6.67*10e-11
+    real             :: G = 6.67 * 10e-11
     real             :: Force
     
-    Force = G * (m1 * m2 / d **2)
+    Force = G * (m1 * m2 / d**2)
 end subroutine f_grav
 
 program n_body
-    use ISO_FORTRAN_ENV, only : REAL128
+    use ISO_FORTRAN_ENV, only: REAL128
     implicit none
     character(len=*), parameter :: OUT_FILE = 'n_body.txt' ! Output file.
-    integer,          parameter :: N = 10, Cycle = 10    ! Number of particles & Cycles         
+    integer, parameter          :: N = 100, Cycle = 10             ! Number of particles & Cycles         
     integer                     :: i, j, fu 
 
     real                        :: u1(N), u2(N), mu_mass, sigma_mass, z_BM(N), mass(N)
     real                        :: pi = 4 * atan(1.0)
 
     real                        :: x(N), y(N), z(N)
-    real                        :: distance_values(N)
-    real, dimension(N)          :: vx, vy, vz, x1, y1, z1, x2, y2, z2
+    real, dimension(N*(N+1))    :: distance_values      ! Array to store distance values between particles
+    real, dimension(N)          :: vx, vy, vz
 
-    ! Creating Mass Array for Objects - Standard Distribition 
+    ! Creating Mass Array for Objects - Standard Distribution 
     call random_number(u1)
     call random_number(u2)
 
@@ -49,34 +49,35 @@ program n_body
     z = z * 1000 + 1 
 
     ! Creating initial velocities
-    vx = abs(vx * 0)
-    vy = abs(vy * 0)
-    vz = abs(vz * 0)
+    vx = 0.0
+    vy = 0.0
+    vz = 0.0
 
-    
-    open (newunit=fu, file=OUT_FILE, status='replace', action='write', form='formatted')       
-        do i = 1, N
-            write(fu, *) mass(i), x(i), y(i), z(i), vx(i), vy(i), vz(i)
-            x1 = x(i)
-            y1 = y(i)
-            z1 = z(i)
-
-            do j = i, N
-                x2 = x(j)
-                y2 = y(j)
-                z2 = z(j)
-
-                call distance(x1,y1,z1,x2,y2,z2,distance_values)
-            end do 
-
+    ! Open output file
+    open(newunit=fu, file=OUT_FILE, status='replace', action='write', form='formatted')       
+    do i = 1, N
+        write(fu, *) mass(i), x(i), y(i), z(i), vx(i), vy(i), vz(i)
+            
+        do j = 1, N
+            if (j /= i) then
+            ! Call the distance calculation subroutine and store the result in distance_values
+            call distance(x(i), y(i), z(i), x(j), y(j), z(j), distance_values(j*(j+1) + i))
+            end if
         end do
-
-    
-        
-    
+    end do
+   
     close(fu)
 
+    ! Print the distance values
+    do i = 1, N
+        do j = 1, N
+            if (j /= i) then
+            write(*, '(A, I3, A, I3, A, F12.6)') "Distance between particles ", i, " and ", j, ": ", distance_values(j*(j+1) + i)
+            end if
+        end do
+    end do
 end program n_body
+
 
 
 
